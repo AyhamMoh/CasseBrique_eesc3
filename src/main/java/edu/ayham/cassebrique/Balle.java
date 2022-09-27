60f6dff1254d0f80fb10c5ffe48c4baf52c8a814
Ayham <ayhammoh87@gmail.com>
2022-09-27 10:44:53 +0200

    Ayham idex frame

---
+++ src/main/java/edu/ayham/cassebrique/Balle.java
@@ -0,0 +1,147 @@
+package edu.ayham.cassebrique;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Balle {
+    protected int x;
+    protected int y;
+    protected int vitesseHorizontal;
+    protected int vitesseVertical;
+    protected int diametre;
+    protected int diametreReflet;
+    protected int decalageReflet;
+    protected Color colour;
+
+    protected ArrayList<Balle> listePoints = new ArrayList<>();
+    protected long indexFrame = 0;
+
+    public Balle(int x, int y, int vtisseHorizontal, int vitesseVertical, int diametre, Color colour) {
+        this.x = x;
+        this.y = y;
+        this.vitesseHorizontal = vtisseHorizontal ==0?1:vtisseHorizontal;
+        this.vitesseVertical = vitesseVertical==0?1:vitesseVertical;
+        this.diametre = diametre;
+        this.colour = colour;
+       this.setDiametre(diametre); //attention!
+    }
+
+    public Balle(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.colour = Color.MAGENTA;
+        this.setDiametre(5);
+    }
+
+
+    public void deplacer (){
+        x += vitesseHorizontal;
+        y += vitesseVertical;
+    } //attention
+
+    public void inverseVitesseHreozontal() {
+        vitesseHorizontal*=-1;
+    } //attention
+
+    public void inverseVitesseVertical() {
+        vitesseVertical*=-1;
+    } //attention
+
+    public void dessiner (Graphics2D dessin) {
+        indexFrame++;
+        if (indexFrame %10 ==0 && vitesseVertical != 0 && vitesseHorizontal!=0) {
+            listePoints.add(new Balle(x,y)) ;
+        }
+
+        dessin.setColor(colour);
+        dessin.fillOval(x,y,diametre,diametre);
+        dessin.setColor(Color.WHITE);
+        dessin.fillOval(x +decalageReflet,y+decalageReflet,diametreReflet,diametreReflet);
+        for (Balle balle :listePoints
+             ) {
+            balle.dessiner(dessin);
+        }
+    }
+
+
+    public void testCollisition (int largeurEcran, int hauteurgeurEcran){
+        if (x<0 ||x>largeurEcran -diametre) {
+            inverseVitesseHreozontal();
+        }
+        if (y<0 ||y>hauteurgeurEcran -diametre) {
+           inverseVitesseVertical();
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getVtesseHorizontal() {
+        return vitesseHorizontal;
+    }
+
+    public void setVtesseHorizontal(int vtesseHorizontal) {
+        this.vitesseHorizontal = vtesseHorizontal;
+    }
+
+    public int getVitesseVertical() {
+        return vitesseVertical;
+    }
+
+    public void setVitesseVertical(int vitesseVertical) {
+        this.vitesseVertical = vitesseVertical;
+    }
+
+    public int getDiametre() {
+        return diametre;
+    }
+
+    public void setDiametre(int diametre) {
+        this.diametre = diametre;
+        this.diametreReflet= (int) (diametre*0.3f); //attention!
+        this.decalageReflet= (int) (diametre*0.2f); //attention!
+    }
+
+    public int getDiametreReflet() {
+        return diametreReflet;
+    }
+
+
+    public int getDecalageReflet() {
+        return decalageReflet;
+    }
+
+
+
+    public Color getColour() {
+        return colour;
+    }
+
+    public void setColour(Color colour) {
+        this.colour = colour;
+    }
+
+    public ArrayList<Balle> getListePoints() {
+        return listePoints;
+    }
+
+    public void setListePoints(ArrayList<Balle> listePoints) {
+        this.listePoints = listePoints;
+    }
+
+
+
+}
--- src/main/java/edu/ayham/cassebrique/CasseBrique.java
+++ src/main/java/edu/ayham/cassebrique/CasseBrique.java
@@ -1,15 +1,24 @@
+package edu.ayham.cassebrique;
+
+import edu.ayham.cassebrique.Balle;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class CasseBrique extends Canvas {
 
+    protected int largeurEcran = 500;
+    protected int hauteurgeurEcran = 700;
     public CasseBrique() throws InterruptedException {
+
+
         JFrame fenetre = new JFrame("Casse brique");
         //On récupère le panneau de la fenetre principale
         JPanel panneau = (JPanel) fenetre.getContentPane();
         //On définie la hauteur / largeur de l'écran
-        panneau.setPreferredSize(new Dimension(500, 500));
-        setBounds(0, 0, 500,500);
+        panneau.setPreferredSize(new Dimension(largeurEcran, hauteurgeurEcran));
+        setBounds(0, 0, largeurEcran,hauteurgeurEcran);
         //On ajoute cette classe (qui hérite de Canvas) comme composant du panneau principal
         panneau.add(this);
 
@@ -31,42 +40,103 @@ public class CasseBrique extends Canvas {
     public void demarrer() throws InterruptedException {
 
         long indexFrame = 0;
-        int xBalle = 250;
-        int yBalle = 250;
-        int vitesseHorizontalBalle = -4;
-        int vitesseVerticalalBalle = 6;
-        int diametreBalle = 50;
-        int diametreRefletBalle = diametreBalle*30/100;
-        //ou alors/ int diametreRefletBalle = 50*0.3f;
-        int decalageReflet = diametreBalle*20/100;
 
+        ArrayList<Balle> listBalles = new ArrayList<>();
+        for (int i = 0; i<5; i++)
+        {
+            listBalles.add(new Balle(
+                    (int)(Math.random()*largeurEcran),
+                    (int)(Math.random()*hauteurgeurEcran),
+                    (int)(Math.random()*5),
+                    (int)(Math.random()*5),
+                    (int)(Math.random()*25),
+                    new Color((float) Math.random(),(float) Math.random(),(float) Math.random())));
+        }
+
+
+
+       /*Balle balle = new Balle(
+               250,
+               250,
+               4,
+               -6,
+               30,
+               Color.RED);
+
+        Balle balle1 = new Balle(
+                250,
+                250,
+                4,
+                -5,
+                30,
+                Color.GREEN);
+
+        Balle balle2 = new Balle(
+                250,
+                250,
+                3,
+                -6,
+                30,
+                Color.BLUE);*/
 
         while(true) {
             indexFrame ++;
+
+
             Graphics2D dessin = (Graphics2D) getBufferStrategy().getDrawGraphics();
 
             //-----------------------------
             //reset dessin
             dessin.setColor(Color.WHITE);
-            dessin.fillRect(0,0,500,500);
+            dessin.fillRect(0,0,largeurEcran,hauteurgeurEcran);
 
             //dessin balle
-            xBalle += vitesseHorizontalBalle;
-            yBalle +=vitesseVerticalalBalle;
 
-            dessin.setColor(Color.RED);
-            dessin.fillOval(xBalle,yBalle, diametreBalle,diametreBalle);
+            for (Balle balle:listBalles
+            ) {
+                balle.deplacer();
+                balle.dessiner(dessin);
+                balle.testCollisition(largeurEcran,hauteurgeurEcran);
+            }
 
-            dessin.setColor(Color.WHITE);
-            dessin.fillOval(xBalle + decalageReflet,yBalle + decalageReflet, decalageReflet,diametreRefletBalle);
+
+           /* balle.deplacer();
+            balle1.deplacer();
+            balle2.deplacer();
+            balle.dessiner(dessin);
+            balle1.dessiner(dessin);
+            balle2.dessiner(dessin);*/
+
+            //balle.setX(balle.getX()+balle.getVtesseHorizontal());
+            //balle.setY(balle.getY()+balle.getVitesseVertical());
+
+
+            //dessin.setColor(balle.getColour());
+            //dessin.fillOval(balle.getX(),balle.getY(), balle.getDiametre(),balle.getDiametre());
+
+            //dessin.setColor(Color.WHITE);
+            //dessin.fillOval(balle.getX() + balle.getDecalageReflet(), balle.getY() +balle.getDecalageReflet(), balle.getDiametreReflet() , balle.getDiametreReflet());
 
             //mouvement balle
-            if (xBalle<0 || xBalle>500 -diametreBalle){vitesseHorizontalBalle*=-1;
-                //vitesseHorizontalBalle=*vitesseHorizontalBalle*-1
-            }
-            if (yBalle<0 || yBalle>500-diametreBalle){vitesseVerticalalBalle*=-1;
-            }
 
+            //balle.tesrcollision
+            /*balle.testCollisition();
+            balle1.testCollisition();
+            balle2.testCollisition();*/
+
+           /* if (balle.getX()<0 || balle.getX()>500 -balle.getDiametre()){balle.inverseVitesseHreozontal();
+                //if (balle.getX()<0 || balle.getX()>500 -balle.getDiametre()){balle.setVtesseHorizontal(balle.getVtesseHorizontal()*-1);
+                    //vitesseHorizontalBalle=*vitesseHorizontalBalle*-1
+            }
+            if (balle.getY()<0 || balle.getY()>700-balle.getDiametre()){balle.inverseVitesseVertical();
+            }*/
+
+
+          /*  if (balle1.getX()<0 || balle1.getX()>500 -balle1.getDiametre()) {
+                balle1.inverseVitesseHreozontal();
+            }
+            if (balle1.getY()<0 || balle1.getY()>700-balle1.getDiametre()){balle1.inverseVitesseVertical();
+            }*/
 
 
 
